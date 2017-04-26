3b146d129ec80ee9a3d938ba1e09257f17c81619
HighnessE <weiyeweb@126.com>
2017-04-26 18:16:55 +0800

    增加部分页面和样式修改

--- 前端页面/js/index.js
+++ 前端页面/js/index.js
@@ -75,28 +75,30 @@ function checkForm(){
         }
 	}
 $(function(){
-	$('#goCity').on('focus',function(){
-	$('.citylist').css('display','block');
-	});
-	
-//	登录验证
-	var user=$('#username').val();
-	var psw=$('#password').val();
-	
-//	var changed=function(id,fn){
-//		id.on('change',fn);
-//	}
-//	var setfeedback = function(id, feedback){
-//		id.addClass('feedback');
 	
 
 })
 
-//联系作者弹出框
+
 $(function(){
+	//联系作者弹出框
 	$("#author").popover({
 		trigger : 'click',//鼠标以上时触发弹出提示框
         html:true,//开启html 为true的话，data-content里就能放html代码了
         content:'<img src="images/qrcode.jpg" height="90" width="90">'
 	});
+	
+	//合作伙伴弹出框
+	$("#friend").popover({
+		trigger : 'click',//鼠标以上时触发弹出提示框
+        html:true,//开启html 为true的话，data-content里就能放html代码了
+        content:'<p>helloCG</p><P>Email:69676900@qq.com</p>'
+	});
+	
+	
+	$('#goCity').on('focus',function(){
+	$('.citylist').css('display','block');
+	});
+	
+	
 })
